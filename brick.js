ea854397da6d13a3ae7fcd57a9af3bd71eda96ba
Chhekur <820121223505e@gmail.com>
2019-06-26 21:54:13 +0530

    Add: Bricks

---
+++ brick.js
@@ -0,0 +1,22 @@
+class Brick {
+    constructor(x, y, w, h, color, on_screen){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.color = color;
+        this.on_screen = on_screen;
+    }
+
+    draw(){
+        fill(this.color);
+        rect(this.x, this.y, this.w, this.h);
+    }
+
+    update(){
+        if((parseInt(ball.x) >= this.x && parseInt(ball.x) <= this.x + this.w) && parseInt(ball.y) == this.y + this.h){
+            this.on_screen = false;
+            console.log('collision');
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 var player;
 var ball;
+var bricks = [];
+var no_of_bricks = 15;
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
     player = new Player(0, height - 20, 100, 10, random_color(), 10);
     ball = new Ball(20, 20 , 15, 5, 120, random_color());
+    generate_bricks();
 }
 
+
+function generate_bricks(){
+    let x = -50, y = 20, w = 50, h = 10;
+    for(let i = 0; i < no_of_bricks; i++){
+        x += w;
+        let brick = new Brick(x, y, w, h, random_color(), true);
+        bricks.push(brick);
+    }
+}
+
+function draw_bricks(){
+    for(let i = 0; i < bricks.length; i++){
+        if(bricks[i].on_screen){
+            bricks[i].draw();
+            bricks[i].update();
+        }
+    }
+}
 
 function random_color(){
     return color(random(255), random(255), random(255));
@@ -25,5 +46,6 @@ function draw(){
     player.update();
     ball.draw();
     ball.update();
+    draw_bricks();
 }
 // basket.x = constrain(mouseX, 0 ,width);
